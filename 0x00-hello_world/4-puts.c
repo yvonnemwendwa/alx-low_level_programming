84323edc7ebbd785faa5527a275144228b2a50dd
yvonnemwendwa <yvonnemwenda17@gmail.com>
2023-03-16 12:41:23 -0700

    Hello, printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+1 #include <stdio.h>
+2
+3  /**
+4  *main - this is the main function
+5  *
+6  *Return: always 0 (success)
+7  */
+8
+9  int main(void)
+10 {
+11	puts("\"Programming is like building a multilingual puzzle");
+12	return(0);
+13  }  	
