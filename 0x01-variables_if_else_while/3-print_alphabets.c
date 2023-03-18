0b35460d947f96f68bd49a8f57d9a25edb0bf067
yvonnemwendwa <yvonnemwenda17@gmail.com>
2023-03-18 05:44:01 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ *
+ *Return: always: 0
+ *
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar (x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar (x);
+	}
+	putchar ('\n');
+	return (0);
+}
